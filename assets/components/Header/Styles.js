639d0c099fa77e880d6fc9171d084e555707f67a
OMAR-FARUK-SHAKIL <omarshakil8@gmail.com>
2021-06-08 02:55:27 +0600

    home button added

--- assets/components/Header/Styles.js
+++ assets/components/Header/Styles.js
@@ -5,16 +5,22 @@ import { Text, StyleSheet, View } from 'react-native'
 const styles = StyleSheet.create({
     container: {
         position: 'absolute',
-        top:50,
-        zIndex: 100,
+        top:'50',
+        zIndex: '100',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        width: '100%',
+        padding: '20'
     },
     logo: {
-        width:100,
-        height:20,
+        width:'100',
+        height:'20',
+        resizeMode:'contain'
     },
     menu: {
-        width:25,
-        height:20,
+        width:'25',
+        height:'20',
+        resizeMode:'contain'
     }
 })
 
